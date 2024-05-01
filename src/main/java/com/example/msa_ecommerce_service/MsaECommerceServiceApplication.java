c3d8acf78377d472662e50382fe1d50d43ff4dad
jaejung <podosi158@gmail.com>
2024-05-01 19:41:36 +0900

    feat: 모듈 분리
    모듈을 Order, Product, User 3개로 분리함

---
+++ src/main/java/com/example/msa_ecommerce_service/MsaECommerceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.msa_ecommerce_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaECommerceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaECommerceServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/msa_ecommerce_service/MsaECommerceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msa_ecommerce_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaECommerceServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
