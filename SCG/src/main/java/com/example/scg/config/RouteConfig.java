a21a19518e17bd6c6f09c67b7254ab983e90c5b7
jaejung <podosi158@gmail.com>
2024-05-10 01:15:03 +0900

    fix: routing path수정

--- SCG/src/main/java/com/example/scg/config/RouteConfig.java
+++ SCG/src/main/java/com/example/scg/config/RouteConfig.java
@@ -12,7 +12,7 @@ public class RouteConfig {
     public RouteLocator ms1Route(RouteLocatorBuilder builder) {
 
         return builder.routes()
-                .route("user", r -> r.path("/user/**")
+                .route("user", r -> r.path("/user/**","/login","/logout")
                         .uri("lb://USER")) //로드 밸런싱을 통해 user서버가 scale out 되어도 scale out 된 서버를 eureka에서 찾아서 접속한다.
                 .route("order", r -> r.path("/order/**")
                         .uri("lb://ORDER"))
