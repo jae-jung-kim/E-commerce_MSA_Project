e3f047cf4a4d3f2d83e85b6be714ad3a599dd0cf
jaejung <podosi158@gmail.com>
2024-06-03 18:18:54 +0900

    fix: user test에 포트번호도 보이게 수정

--- UserService/src/main/java/com/example/userservice/member/controller/MemberController.java
+++ UserService/src/main/java/com/example/userservice/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.example.userservice.member.dto.MemberForm;
 import com.example.userservice.member.entity.Member;
 import com.example.userservice.member.service.MemberService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +15,10 @@ import org.springframework.web.bind.annotation.*;
 public class MemberController {
 
     private final MemberService memberService;
-
-    public MemberController(MemberService memberService) {
+    private final Environment env;
+    public MemberController(MemberService memberService, Environment env) {
         this.memberService = memberService;
+        this.env = env;
     }
 
     @PostMapping("/create")
@@ -44,6 +46,6 @@ public class MemberController {
 
     @GetMapping
     public String mainP(){ //test
-        return "user Controller";
+        return String.format("user Controller Port %s", env.getProperty("local.server.port"));
     }
 }
