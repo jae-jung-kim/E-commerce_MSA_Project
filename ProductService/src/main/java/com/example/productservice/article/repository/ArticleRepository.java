a984976d486fd66f001a942991287a284c85f626
jaejung <podosi158@gmail.com>
2024-06-06 20:30:00 +0900

    feat: 상품 서비스 appConfig작성

--- ProductService/src/main/java/com/example/productservice/article/repository/ArticleRepository.java
+++ ProductService/src/main/java/com/example/productservice/article/repository/ArticleRepository.java
@@ -2,10 +2,11 @@ package com.example.productservice.article.repository;
 
 import com.example.productservice.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ArticleRepository extends JpaRepository <Article,Long>{
+public interface ArticleRepository extends JpaRepository<Article,Long> {
 
 }
