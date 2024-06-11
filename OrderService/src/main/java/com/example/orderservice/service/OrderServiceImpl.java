22ddafb55469ef31a726bf43e3db51dab0e7b6d0
jaejung <podosi158@gmail.com>
2024-06-11 15:07:25 +0900

    feat: order service 구현

---
+++ OrderService/src/main/java/com/example/orderservice/service/OrderServiceImpl.java
@@ -0,0 +1,53 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.config.AppConfig;
+import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.jpa.OrderRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class OrderServiceImpl implements OrderService{
+
+    OrderRepository orderRepository;
+    AppConfig appConfig;
+    @Autowired
+    public OrderServiceImpl(OrderRepository orderRepository, AppConfig appConfig) {
+        this.orderRepository = orderRepository;
+        this.appConfig = appConfig;
+    }
+
+    @Override
+    public OrderDto createOrder(OrderDto orderDto) {
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(orderDto.getQty() * orderDto.getUnitPrice());
+
+        ModelMapper mapper = appConfig.modelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        OrderEntity orderEntity = mapper.map(orderDto, OrderEntity.class);
+
+        orderRepository.save(orderEntity);
+
+        OrderDto returnValue = mapper.map(orderEntity, OrderDto.class);
+
+        return returnValue;
+    }
+
+    @Override
+    public OrderDto getOrderByOrderId(String orderId) {
+        OrderEntity orderEntity = orderRepository.findByOrderId(orderId);
+        OrderDto orderDto = appConfig.modelMapper().map(orderEntity, OrderDto.class);
+
+        return orderDto;
+    }
+
+    @Override
+    public Iterable<OrderEntity> getOrdersByUserId(String userId) {
+        return orderRepository.findByUserId(userId);
+    }
+}
