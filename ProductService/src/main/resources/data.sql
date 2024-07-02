29e0524168236b38c303ab258b1e7358a28c8e55
jaejung <podosi158@gmail.com>
2024-07-02 20:11:51 +0900

    fix: table이름에 맞게 수정

--- ProductService/src/main/resources/data.sql
+++ ProductService/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-insert into article(name, product_id, price, quantity, created_at) values('컵라면', '1', 3000, 30, now());
+insert into articles(name, product_id, price, quantity, created_at) values('컵라면', '1', 3000, 30, now());
 
-insert into article(name, product_id, price, quantity, created_at) values('김밥', '2', 1000, 50, now());
+insert into articles(name, product_id, price, quantity, created_at) values('김밥', '2', 1000, 50, now());
 
-insert into article(name, product_id, price, quantity, created_at) values('볼펜', '3', 500, 100, now());
+insert into articles(name, product_id, price, quantity, created_at) values('볼펜', '3', 500, 100, now());
